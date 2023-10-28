cd2d4fcb5eb0029e7d931205e2b432c7252140a7
Petros-Emmanouilidis <113743927+Petros-Emmanouilidis@users.noreply.github.com>
2023-10-28 22:06:38 +0300

    Update project.sv
    Was assigning goofy inputs lmao. Hopefully, it will work now

--- src/project.sv
+++ src/project.sv
@@ -14,9 +14,7 @@ module tt_um_tiny_game_of_life (
     logic reset;
     FSM Petros_Game_of_Life(.in(ui_in[1:0]), .out(uo_out[2:0]), .clock(ui_in[2]), .*);
     assign reset = ~rst_n;
-    assign ui_in[7:3] = '0;
     assign uo_out[7:3] = '0;
-    assign uio_in[7:0] = '0;
     assign uio_out[7:0] = '0;
     assign uio_oe[7:0] = '0;
     
